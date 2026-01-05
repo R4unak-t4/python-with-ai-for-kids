041c0d5e5f202a873c84f4de0b38f38ac13c7a29
R4unak-t4 <thapamagarraunak@gmail.com>
2026-01-05 18:33:00 +0545

    First

---
+++ Week2/Day4.py
@@ -0,0 +1,20 @@
+# function
+
+# vowels = ('a','e','i','o','u')
+
+# def vowels_count(word):
+#     count = 0
+#     for char in word:
+#         if char.lower() not in vowels:
+#             count+=1
+#     return count
+#
+# print(vowels_count("rAunak"))
+
+'''
+Write a Python function called find_max_min that takes a list of integers as a parameter and returns both the maximum and minimum values in the list without using the built-in max() or min() functions.
+
+Example:
+Input: [4, 7, 1, 9, 2]
+Output: (9, 1)
+'''
