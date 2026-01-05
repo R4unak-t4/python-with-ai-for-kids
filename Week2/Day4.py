9be8a111aedfc7f02c0d7f2ff2da92e54175267e
R4unak-t4 <thapamagarraunak@gmail.com>
2026-01-05 19:14:34 +0545

    First

--- Week2/Day4.py
+++ Week2/Day4.py
@@ -18,3 +18,33 @@ Example:
 Input: [4, 7, 1, 9, 2]
 Output: (9, 1)
 '''
+
+# def min_max(arr):
+#     min = arr[0]
+#     max = arr[0]
+#     for i in arr:
+#         if i < min :
+#             min = i
+#         if i > max:
+#             max = i
+#     return (max,min)
+#
+# print(min_max([4, 7, 1, 9, 2]))
+
+'''
+Write a Python function called remove_duplicates that takes a list of numbers as a parameter and returns a new list with all duplicate values removed, while keeping the original order.
+
+Example:
+Input: [1, 2, 2, 3, 4, 3, 5]
+Output: [1, 2, 3, 4, 5]
+'''
+
+# def el_dup(arr):
+#     li = []
+#     for i in arr:
+#         if i not in li:
+#             li.append(i)
+#     return li
+#
+# print(el_dup([1, 2, 2, 3, 4, 3, 5]))
+
---
+++ side/comparison.py
@@ -0,0 +1,25 @@
+# >
+# print(3 > 2)
+
+# <
+# print(5 < 5)
+
+# ==
+# print(5 == 5)
+
+# <= (less than or equal to)
+# print(6 <= 5)
+
+# >= (greater than or equal to)
+# print(6 >=5)
+
+# == for str
+# print('Shivaay' == 'shivaay'.title())
+#
+# name = 'SHivAAY'
+# print(name.lower())
+
+# Task : use all the comparison operators for float
+
+# Take first name and last name as input and covert it to title case and print it
+
